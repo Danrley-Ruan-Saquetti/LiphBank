39a05a707fdd58ad759d043775b91f6e1399dfed
Dan Ruan <danrsaquetti@gmail.com>
2024-12-07 15:48:16 -0300

    feat: Add validation fields in people create use case

--- server/src/app/modules/people/@tests/create.spec.ts
+++ server/src/app/modules/people/@tests/create.spec.ts
@@ -1,15 +1,16 @@
 import { describe, expect, test, assert } from 'vitest'
 import { People, PeopleCreateUseCase } from '../use-cases/create'
+import { ValidationException } from '../../../../adapters/validator/validation.exception'
 
 describe('Create People', () => {
 
   test('Simple create people', async () => {
     const arrange = {
       name: 'Dan Ruan',
-      itinCnpj: '102.547.108-13',
+      itinCnpj: '102.547.109-13',
       genre: 'M',
       dateOfBirth: new Date('2004-05-28 00:00:00'),
-      type: 'P',
+      type: 'NP',
     }
 
     const createPeople = new PeopleCreateUseCase()
@@ -17,6 +18,7 @@ describe('Create People', () => {
     const response = await createPeople.perform(arrange)
 
     expect(response.user).toBeInstanceOf(People)
+    expect(response.user.id).equal(1)
   })
 
   test('Invalid name empty', async () => {
@@ -25,11 +27,21 @@ describe('Create People', () => {
       itinCnpj: '102.547.108-13',
       genre: 'M',
       dateOfBirth: new Date('2004-05-28 00:00:00'),
-      type: 'P',
+      type: 'NP',
     }
 
     const createPeople = new PeopleCreateUseCase()
 
-    await expect(createPeople.perform(arrange)).rejects.toThrow('Name is required')
+    await expect(async () => {
+      try {
+        await createPeople.perform(arrange)
+      } catch (error) {
+        expect(error.message).equal('Invalid data')
+
+        throw error
+      }
+    })
+      .rejects
+      .toThrow(ValidationException)
   })
 })
--- server/src/app/modules/people/use-cases/create.ts
+++ server/src/app/modules/people/use-cases/create.ts
@@ -1,6 +1,9 @@
 import { z } from 'zod'
+import { ZodValidatorAdapter } from '../../../../adapters/validator'
+import { validateCnpj, validateItin } from '../../../../helpers/itin-cnpj'
 
 export class People {
+  id: number
   name: string
   itinCnpj: string
   genre: string
@@ -9,27 +12,49 @@ export class People {
 }
 
 const userCreateSchema = z.object({
-  name: z.string({ 'required_error': 'Name is required' }),
-  itinCnpj: z.string(),
-  genre: z.string(),
-  type: z.string(),
-  dateOfBirth: z.coerce.date(),
+  name: z
+    .string({ 'required_error': 'Name is required' })
+    .trim()
+    .min(1),
+  itinCnpj: z
+    .string()
+    .trim()
+    .default('PE'),
+  genre: z
+    .string()
+    .trim(),
+  type: z
+    .string()
+    .trim(),
+  dateOfBirth: z
+    .coerce
+    .date(),
 })
+  .refine(({ type, itinCnpj }) => {
+    if (type == 'NP') {
+      return validateItin(itinCnpj)
+    }
+
+    return type == 'LE' && validateCnpj(itinCnpj)
+  }, ({ type }) => ({ message: `${type == 'LE' ? 'CNPJ' : 'ITIN'} invalid`, path: ['itinCnpj'] }))
 
 export type PeopleCreateUseCaseProps = z.input<typeof userCreateSchema>
 
 export class PeopleCreateUseCase {
 
+  private validator = new ZodValidatorAdapter()
+
   async perform(args: PeopleCreateUseCaseProps) {
-    const { dateOfBirth, genre, itinCnpj, name, type } = userCreateSchema.parse(args)
+    const dto = this.validator.validate(userCreateSchema, args)
 
     const people = new People()
 
-    people.name = name
-    people.itinCnpj = itinCnpj
-    people.genre = genre
-    people.dateOfBirth = dateOfBirth
-    people.type = type
+    people.id = 1
+    people.name = dto.name
+    people.itinCnpj = dto.itinCnpj
+    people.genre = dto.genre
+    people.dateOfBirth = dto.dateOfBirth
+    people.type = dto.type
 
     return {
       user: people
