07fc13efe36a55b86844f0ec2137c59bb605add4
Dan Ruan <danrsaquetti@gmail.com>
2024-12-07 15:42:46 -0300

    feat: Creating adapter itin/cnpj with tests

---
+++ server/src/@tests/itin-cnpj.spec.ts
@@ -0,0 +1,106 @@
+import { describe } from 'vitest'
+import { validateCnpj, validateItin } from '../helpers/itin-cnpj'
+
+describe('Validate ITIN', () => {
+  test('ITIN Valid with dots', () => {
+    const arrange = [
+      '102.547.109-13',
+      '435.869.570-17',
+      '886.509.060-01',
+      '514.591.690-69',
+      '008.806.530-86',
+      '045.044.030-30',
+      '699.199.890-70',
+    ]
+
+    arrange.map(itin => {
+      expect(validateItin(itin)).toEqual(true)
+    })
+  })
+
+  test('ITIN Valid without dots', () => {
+    const arrange = [
+      '10254710913',
+      '43586957017',
+      '88650906001',
+      '51459169069',
+      '00880653086',
+      '04504403030',
+      '69919989070',
+    ]
+
+    arrange.map(itin => {
+      expect(validateItin(itin)).toEqual(true)
+    })
+  })
+
+  test('ITIN Invalid in format', () => {
+    const arrange = [
+      '023.542.787-13',
+      '31.23245.3213',
+      '4598.3612973',
+      '8967.98808',
+      '89.s7d67s89 668',
+      '',
+      '1231',
+      'sfdghh.bngbf',
+      '00000000000',
+      '000.000.000-00',
+    ]
+
+    arrange.map(itin => {
+      expect(validateItin(itin)).toEqual(false)
+    })
+  })
+})
+
+describe('Validate CNPJ', () => {
+  test('CNPJ Valid with dots', () => {
+    const arrange = [
+      '93.892.889/0001-40',
+      '22.062.665/0001-62',
+      '17.956.873/0001-75',
+      '76.969.184/0001-64',
+      '91.857.377/0001-17',
+      '75.926.288/0001-29',
+      '02.346.671/0001-34',
+    ]
+
+    arrange.map(itin => {
+      expect(validateCnpj(itin)).toEqual(true)
+    })
+  })
+
+  test('CNPJ Valid without dots', () => {
+    const arrange = [
+      '93892889000140',
+      '22062665000162',
+      '17956873000175',
+      '76969184000164',
+      '91857377000117',
+      '75926288000129',
+      '02346671000134',
+    ]
+
+    arrange.map(itin => {
+      expect(validateCnpj(itin)).toEqual(true)
+    })
+  })
+
+  test('CNPJ Invalid in format', () => {
+    const arrange = [
+      '43.124.654/1232-32',
+      '',
+      '2',
+      '43423.42342342.3-2',
+      '123131312313131231',
+      '02346671000131',
+      '00000000000000',
+      '00.000.000/0000-00',
+    ]
+
+    arrange.map(itin => {
+      expect(validateCnpj(itin)).toEqual(false)
+    })
+  })
+})
---
+++ server/src/helpers/itin-cnpj.ts
@@ -0,0 +1,76 @@
+export function formatItin(value: string) {
+  return value.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})$/, '$1.$2.$3-$4')
+}
+
+export function formatCnpj(value: string) {
+  return value.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, '$1.$2.$3/$4-$5')
+}
+
+export function validateFormatItin(value: string) {
+  return /^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(value)
+}
+
+export function validateFormatCnpj(value: string) {
+  return /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/.test(value)
+}
+
+export function validateItin(value: string) {
+  if (!value) {
+    return false
+  }
+
+  value = value.replace(/[^\d]+/g, '')
+
+  if (value.length !== 11 || /^(\d)\1+$/.test(value)) {
+    return false
+  }
+
+  const calc = (digit: number) =>
+    value
+      .slice(0, digit)
+      .split('')
+      .reduce((sum, num, index) => sum + parseInt(num) * (digit + 1 - index), 0)
+
+  const digit1 = (calc(9) * 10) % 11 % 10
+  const digit2 = (calc(10) * 10) % 11 % 10
+
+  return digit1 === parseInt(value[9]) && digit2 === parseInt(value[10])
+}
+
+export function validateCnpj(value: string = '') {
+  if (!value) return false
+
+  value = value.replace(/[^\d]+/g, '')
+
+  const numbers = matchNumbers(value)
+  if (numbers.length !== 14) return false
+
+  const items = [...new Set(numbers)]
+  if (items.length === 1) return false
+
+  const digit0 = validCalc(12, numbers)
+  const digit1 = validCalc(13, numbers)
+
+  return digit0 === numbers[12] && digit1 === numbers[13]
+}
+
+function validCalc(x: number, numbers: number[]) {
+  let factor = x - 7
+  let sum = 0
+
+  for (let i = 0; i < x; i++) {
+    sum += numbers[i] * factor--
+    if (factor < 2) factor = 9
+  }
+
+  const result = 11 - (sum % 11)
+  return result > 9 ? 0 : result
+}
+
+function matchNumbers(value: string | number | number[] = '') {
+  const match = value
+    .toString()
+    .replace(/[^\d]+/g, '')
+    .match(/\d/g)
+  return Array.isArray(match) ? match.map(Number) : []
+}
