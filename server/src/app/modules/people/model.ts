4178583a6c6291aad5b8167c161e1ee7ab0fe21b
Dan Ruan <danrsaquetti@gmail.com>
2024-12-08 15:22:40 -0300

    feat: Creating method static to create and load object entity

--- server/src/app/modules/people/model.ts
+++ server/src/app/modules/people/model.ts
@@ -1,7 +1,5 @@
 import { People as PeoplePrisma } from '@prisma/client'
 
-export type PeopleProps = PeoplePrisma
-
 export enum PeopleType {
   NATURAL_PERSON = 'NP',
   LEGAL_ENTITY = 'LE',
@@ -12,6 +10,11 @@ export enum PeopleGender {
   FEMININE = 'F',
 }
 
+export interface PeopleProps extends PeoplePrisma {
+  type: PeopleType | null
+  gender: PeopleGender | null
+}
+
 export class People implements PeopleProps {
   static readonly Type = PeopleType
   static readonly Gender = PeopleGender
@@ -20,7 +23,6 @@ export class People implements PeopleProps {
   private _name: string
   private _type: PeopleType
   private _cpfCnpj: string
-  private _active: boolean
   private _gender: PeopleGender | null = null
   private _dateOfBirth: Date | null = null
   private _createdAt: Date
@@ -30,7 +32,6 @@ export class People implements PeopleProps {
   get name() { return this._name }
   get type() { return this._type }
   get cpfCnpj() { return this._cpfCnpj }
-  get active() { return this._active }
   get gender() { return this._gender }
   get dateOfBirth() { return this._dateOfBirth }
   get createdAt() { return this._createdAt }
@@ -40,9 +41,23 @@ export class People implements PeopleProps {
   set name(value) { this._name = value }
   set type(value) { this._type = value }
   set cpfCnpj(value) { this._cpfCnpj = value }
-  set active(value) { this._active = value }
   set gender(value) { this._gender = value }
   set dateOfBirth(value) { this._dateOfBirth = value }
   set createdAt(value) { this._createdAt = value }
   set updatedAt(value) { this._updatedAt = value }
+
+  static load(props: Partial<PeopleProps>) {
+    const people = new People()
+
+    if (props.id) people.id = props.id
+    if (props.name) people.name = props.name
+    if (props.type) people.type = props.type
+    if (props.cpfCnpj) people.cpfCnpj = props.cpfCnpj
+    if (props.gender) people.gender = props.gender
+    if (props.dateOfBirth) people.dateOfBirth = props.dateOfBirth
+    if (props.createdAt) people.createdAt = props.createdAt
+    if (props.updatedAt) people.updatedAt = props.updatedAt
+
+    return people
+  }
 }
