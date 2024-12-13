d18f357f7e2578e5ed1a7fd6999001a03ff50601
Dan Ruan <danrsaquetti@gmail.com>
2024-12-13 20:40:07 -0300

    feat: New method findById in UserRepository

--- server/src/app/modules/user/repository.ts
+++ server/src/app/modules/user/repository.ts
@@ -3,6 +3,7 @@ import { User, UserType } from './model'
 export abstract class UserRepository {
 
   abstract create(user: User): Promise<User>
+  abstract findById(id: number): Promise<User | null>
   abstract findByPeopleIdAndType(peopleId: number, type: UserType): Promise<User | null>
   abstract findByLoginAndType(login: string, type: UserType): Promise<User | null>
 }
