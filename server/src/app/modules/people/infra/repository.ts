0674803dddd645df5fd171548e9c5ba0c9b67431
Dan Ruan <danrsaquetti@gmail.com>
2024-12-15 19:21:04 -0300

    feat: Creating class PeopleRepository implementation

---
+++ server/src/app/modules/people/infra/repository.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common'
+import { PeopleRepository } from '../repository'
+import { People } from '../model'
+
+@Injectable()
+export class PeoplePrismaRepository extends PeopleRepository {
+  create(people: People): Promise<People> {
+    throw new Error('Method not implemented.')
+  }
+  update(id: number, people: People): Promise<People> {
+    throw new Error('Method not implemented.')
+  }
+  delete(id: number): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+  findById(id: number): Promise<People | null> {
+    throw new Error('Method not implemented.')
+  }
+  findMany(): Promise<People[]> {
+    throw new Error('Method not implemented.')
+  }
+  findByCpfCnpj(cpfCnpj: string): Promise<People | null> {
+    throw new Error('Method not implemented.')
+  }
+}
