6833ae301804ed7d5476fb04a924fe051c81b5ae
Dan Ruan <danrsaquetti@gmail.com>
2024-12-19 20:28:24 -0300

    feat: Configuring env

---
+++ server/src/env.ts
@@ -0,0 +1,12 @@
+export type Environment = {
+  ENVIRONMENT: string
+  SECRET_PEPPER: string
+  DATABASE_URL: string
+  JWT_SECRET: string
+  PORT: number
+  TZ: string
+}
+
+export function env<T extends keyof Environment>(name: T, defaultValue?: Environment[T]) {
+  return (process.env[name] ?? defaultValue ?? null) as Environment[T]
+}
