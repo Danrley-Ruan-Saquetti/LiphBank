a7fc2f8e1e54cb174c96ecc6d5e302e0e48cf4d2
Dan Ruan <danrsaquetti@gmail.com>
2024-12-19 21:48:29 -0300

    refactor: Moving env file to shared folder

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { MainModule } from '@main.module'
-import { env } from '@env'
+import { env } from '@shared/env'
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule)
