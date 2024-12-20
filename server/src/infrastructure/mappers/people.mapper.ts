582f7fc1ec5529f48003f0858265802246e07fa1
Dan Ruan <danrsaquetti@gmail.com>
2024-12-19 23:41:07 -0300

    feat: New methods to mapper multi object for PeopleMapper

--- server/src/infrastructure/mappers/people.mapper.ts
+++ server/src/infrastructure/mappers/people.mapper.ts
@@ -3,6 +3,14 @@ import { People, PeopleGender, PeopleType } from '@domain/entities/people.entity
 
 export class PeopleMapper {
 
+  static multiEntityToDatabase(entities: People[]) {
+    return entities.map(entity => PeopleMapper.databaseToEntity(entity))
+  }
+
+  static multiDatabaseToEntity(databaseModels: PeopleModel[]) {
+    return databaseModels.map(databaseModel => PeopleMapper.databaseToEntity(databaseModel))
+  }
+
   static entityToDatabase(entity: People) {
     const peopleDatabase: PeopleModel = {
       id: entity.id,
