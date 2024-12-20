d61e312a8b74a8a26fd06a89e10f7f7f514c2bc0
Dan Ruan <danrsaquetti@gmail.com>
2024-12-19 23:41:26 -0300

    feat: Implementing classe PeopleRepositoryImplementation

--- server/src/domain/repositories/people.repository.ts
+++ server/src/domain/repositories/people.repository.ts
@@ -1,4 +1,8 @@
 import { People } from '@domain/entities/people.entity'
+import { FilterSchema, QuerySchema } from '@domain/database/filters'
+
+export type PeopleFilter = FilterSchema<People>
+export type PeopleQueryArgs = QuerySchema<People>
 
 export abstract class PeopleRepository {
 
@@ -6,6 +10,6 @@ export abstract class PeopleRepository {
   abstract update(id: number, people: People): Promise<People>
   abstract delete(id: number): Promise<void>
   abstract findById(id: number): Promise<People | null>
-  abstract findMany(): Promise<People[]>
+  abstract findMany(args?: PeopleQueryArgs): Promise<People[]>
   abstract findByCpfCnpj(cpfCnpj: string): Promise<People | null>
 }
--- server/src/infrastructure/repositories/people.repository.ts
+++ server/src/infrastructure/repositories/people.repository.ts
@@ -1,24 +1,98 @@
-import { PeopleRepository } from '@domain/repositories/people.repository'
-import { People } from '../../domain/entities/people.entity'
+import { PrismaClient } from '@prisma/client'
+import { PeopleMapper } from '@infrastructure/mappers/people.mapper'
+import { People } from '@domain/entities/people.entity'
+import { PeopleQueryArgs, PeopleRepository } from '@domain/repositories/people.repository'
 
 export class PeopleRepositoryImplementation extends PeopleRepository {
 
-  create(people: People): Promise<People> {
-    throw new Error('Method not implemented.')
+  constructor(
+    private readonly prisma: PrismaClient
+  ) {
+    super()
   }
-  update(id: number, people: People): Promise<People> {
-    throw new Error('Method not implemented.')
+
+  async create(people: People) {
+    try {
+      const peopleModel = PeopleMapper.entityToDatabase(people)
+
+      const peopleDatabase = await this.prisma.people.create({
+        data: {
+          cpfCnpj: peopleModel.cpfCnpj,
+          name: peopleModel.name,
+          gender: peopleModel.gender,
+          type: peopleModel.type,
+          dateOfBirth: peopleModel.dateOfBirth,
+        }
+      })
+
+      return PeopleMapper.databaseToEntity(peopleDatabase)
+    } catch (error: any) {
+
+      throw error
+    }
   }
-  delete(id: number): Promise<void> {
-    throw new Error('Method not implemented.')
+
+  async update(id: number, people: People) {
+    try {
+      const peopleModel = PeopleMapper.entityToDatabase(people)
+
+      const peopleDatabase = await this.prisma.people.update({
+        where: { id },
+        data: {
+          cpfCnpj: peopleModel.cpfCnpj,
+          name: peopleModel.name,
+          gender: peopleModel.gender,
+          type: peopleModel.type,
+          dateOfBirth: peopleModel.dateOfBirth,
+        }
+      })
+
+      return PeopleMapper.databaseToEntity(peopleDatabase)
+    } catch (error: any) {
+
+      throw error
+    }
   }
-  findById(id: number): Promise<People | null> {
-    throw new Error('Method not implemented.')
+
+  async delete(id: number) {
+    try {
+      await this.prisma.people.delete({ where: { id } })
+    } catch (error: any) {
+
+      throw error
+    }
   }
-  findMany(): Promise<People[]> {
-    throw new Error('Method not implemented.')
+
+  async findMany(args: PeopleQueryArgs = {}) {
+    try {
+      const peoplesDatabase = await this.prisma.people.findMany({ ...args } as any)
+
+      return PeopleMapper.multiDatabaseToEntity(peoplesDatabase)
+    } catch (error: any) {
+
+      throw error
+    }
   }
-  findByCpfCnpj(cpfCnpj: string): Promise<People | null> {
-    throw new Error('Method not implemented.')
+
+  async findById(id: number) {
+    try {
+      const peopleDatabase = await this.prisma.people.findUnique({ where: { id } })
+
+      return peopleDatabase ? PeopleMapper.databaseToEntity(peopleDatabase) : null
+    } catch (error: any) {
+
+      throw error
+    }
+  }
+
+  async findByCpfCnpj(cpfCnpj: string) {
+    try {
+      const peopleDatabase = await this.prisma.people.findUnique({ where: { cpfCnpj } })
+
+      return peopleDatabase ? PeopleMapper.databaseToEntity(peopleDatabase) : null
+    } catch (error: any) {
+
+      throw error
+    }
   }
 }
