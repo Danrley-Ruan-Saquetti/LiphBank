7e1da8a06bd53b7f3302f77d4d26a049ed8b0e8d
Dan Ruan <danrsaquetti@gmail.com>
2024-12-20 14:59:55 -0300

    feat: Add tsconfig in vitest config file

--- server/config/test/vitest.config.e2e.ts
+++ server/config/test/vitest.config.e2e.ts
@@ -3,18 +3,15 @@ import swc from 'unplugin-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': new URL('./src', import.meta.url).pathname
-    }
-  },
   test: {
     include: ['**/*.e2e-spec.ts'],
     globals: true,
     root: './',
+  },
+  resolve: {
     alias: {
-      '@': './src'
-    },
+      '@': new URL('./src', import.meta.url).pathname
+    }
   },
   plugins: [
     tsconfigPaths(),
