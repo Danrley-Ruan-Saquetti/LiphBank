288a70767252b9700b5a48f02c737f8d3f26b485
Dan Ruan <danrsaquetti@gmail.com>
2024-12-20 21:24:54 -0300

    feat: Add details and timestamp in Exception class

--- server/src/shared/exceptions/exception.ts
+++ server/src/shared/exceptions/exception.ts
@@ -1,1 +1,11 @@
-export class Exception extends Error { }
+export class Exception extends Error {
+
+  readonly timestamp = new Date(Date.now())
+
+  constructor(
+    message: string,
+    public readonly details: Record<string, any> | null = null
+  ) {
+    super(message)
+  }
+}
