c617d7b68e033229bd0e6ae9867330eeef56780b
Dan Ruan <danrsaquetti@gmail.com>
2024-12-22 16:57:11 -0300

    refactor: Creating file for exporting exceptions

---
+++ server/src/shared/exceptions/index.ts
@@ -0,0 +1,4 @@
+export * from '@shared/exceptions/conflict.exception'
+export * from '@shared/exceptions/critical.exception'
+export * from '@shared/exceptions/not-found.exception'
+export * from '@shared/exceptions/runtime.exception'
