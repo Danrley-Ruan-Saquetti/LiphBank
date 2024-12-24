1498cbe08be2b1104fda41d1501bb1cc984d2846
Dan Ruan <danrsaquetti@gmail.com>
2024-12-23 22:04:53 -0300

    feat: Creating a new DTO UserGenerateCodeDTO

---
+++ server/src/application/dto/user/generate-code.dto.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod'
+
+export const userGenerateCodeSchema = z.object({
+  attempts: z
+    .number()
+    .int()
+    .min(1, 'Number of attempts must be greater than 0 (zero)')
+    .default(3)
+})
+
+export type UserGenerateCodeDTO = z.input<typeof userGenerateCodeSchema>
