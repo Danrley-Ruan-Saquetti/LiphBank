0e90e246652aa4fd671e10ea1404774f025aede7
Dan Ruan <danrsaquetti@gmail.com>
2024-12-25 00:16:47 -0300

    feat: Creating a mapper of the BankAccount

---
+++ server/src/infrastructure/mappers/bank-account.mapper.ts
@@ -0,0 +1,43 @@
+import { BankAccountModel } from '@infrastructure/models/bank-account.model'
+import { BankAccount } from '@domain/entities/bank-account.entity'
+
+export class BankAccountMapper {
+
+  static multiEntityToDatabase(entities: BankAccount[]) {
+    return entities.map(entity => BankAccountMapper.databaseToEntity(entity))
+  }
+
+  static multiDatabaseToEntity(databaseModels: BankAccountModel[]) {
+    return databaseModels.map(databaseModel => BankAccountMapper.databaseToEntity(databaseModel))
+  }
+
+  static entityToDatabase(entity: BankAccount) {
+    const bankAccountDatabase: BankAccountModel = {
+      id: entity.id,
+      name: entity.name,
+      active: entity.active,
+      balance: entity.balance,
+      code: entity.code,
+      createdAt: entity.createdAt,
+      peopleId: entity.peopleId,
+      slug: entity.slug,
+      updatedAt: entity.updatedAt,
+    }
+
+    return bankAccountDatabase
+  }
+
+  static databaseToEntity(databaseModel: BankAccountModel) {
+    return BankAccount.load({
+      id: databaseModel.id,
+      name: databaseModel.name,
+      active: databaseModel.active,
+      balance: databaseModel.balance,
+      code: databaseModel.code,
+      createdAt: databaseModel.createdAt,
+      peopleId: databaseModel.peopleId,
+      slug: databaseModel.slug,
+      updatedAt: databaseModel.updatedAt,
+    })
+  }
+}
