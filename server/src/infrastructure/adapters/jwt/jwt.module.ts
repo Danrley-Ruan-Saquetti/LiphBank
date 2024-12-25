f6ba02fe0609338e0797d775dc7c5ed334589295
Dan Ruan <danrsaquetti@gmail.com>
2024-12-24 22:54:58 -0300

    fix: Import module JwtModule in InfrastructureJWTModule

--- server/src/infrastructure/adapters/jwt/jwt.module.ts
+++ server/src/infrastructure/adapters/jwt/jwt.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
 import { JWTServiceImplementation } from '@infrastructure/adapters/jwt/jwt.service'
-import { JWT } from '@domain/adapters/jwt/index'
+import { JWT } from '@domain/adapters/jwt'
 
 @Module({
+  imports: [
+    JwtModule.register({})
+  ],
   providers: [
     JWTServiceImplementation,
     {
