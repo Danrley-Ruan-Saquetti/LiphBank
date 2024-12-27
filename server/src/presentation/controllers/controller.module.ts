392ddfe42ce3e12fa451957f01a03cbeb329ea66
Dan Ruan <danrsaquetti@gmail.com>
2024-12-27 15:33:27 -0300

    feat: Creating controller FinancialTransaction - /bank-accounts/:id/financial-transactions

--- server/src/presentation/controllers/controller.module.ts
+++ server/src/presentation/controllers/controller.module.ts
@@ -3,11 +3,13 @@ import { UserController } from '@presentation/controllers/user.controller'
 import { AuthController } from '@presentation/controllers/auth.controller'
 import { PeopleController } from '@presentation/controllers/people.controller'
 import { BankAccountController } from '@presentation/controllers/bank-account.controller'
+import { FinancialTransactionController } from '@presentation/controllers/financial-transaction.controller'
 import { UserUseCaseModule } from '@application/use-cases/user/use-case.module'
 import { AuthUseCaseModule } from '@application/use-cases/auth/use-case.module'
 import { PeopleUseCaseModule } from '@application/use-cases/people/use-case.module'
 import { SharedUseCaseModule } from '@application/use-cases/shared/use-case.module'
 import { BankAccountUseCaseModule } from '@application/use-cases/bank-account/use-case.module'
+import { FinancialTransactionUseCaseModule } from '@application/use-cases/financial-transaction/use-case.module'
 
 @Module({
   imports: [
@@ -16,12 +18,14 @@ import { BankAccountUseCaseModule } from '@application/use-cases/bank-account/us
     SharedUseCaseModule,
     AuthUseCaseModule,
     BankAccountUseCaseModule,
+    FinancialTransactionUseCaseModule,
   ],
   controllers: [
     PeopleController,
     UserController,
     AuthController,
     BankAccountController,
+    FinancialTransactionController
   ]
 })
 export class ControllerModule { }
---
+++ server/src/presentation/controllers/financial-transaction.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, HttpCode, HttpStatus, Param, Post, UseGuards } from '@nestjs/common'
+import { AuthGuard } from '@presentation/guards/auth.guard'
+import { FinancialTransactionCreateUseCase } from '@application/use-cases/financial-transaction/create.use-case'
+
+@Controller('/bank-accounts/:id/financial-transactions')
+export class FinancialTransactionController {
+
+  constructor(
+    private readonly financialTransactionCreateUseCase: FinancialTransactionCreateUseCase
+  ) { }
+
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.CREATED)
+  @Post('/create')
+  async create(@Body() body: any, @Param('id') bankAccountId: string) {
+    await this.financialTransactionCreateUseCase.perform({ ...body, bankAccountId: +bankAccountId })
+
+    return { message: 'Bank Account successfully created' }
+  }
+}
