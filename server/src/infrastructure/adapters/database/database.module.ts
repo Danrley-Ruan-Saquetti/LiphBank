7534d32b76d4f34b79ea2d4bd7ad171fae2cc4f4
Dan Ruan <danrsaquetti@gmail.com>
2024-12-26 22:30:26 -0300

    refactor: Separate providers to array in InfrastructureDatabaseModule

--- server/src/infrastructure/adapters/database/database.module.ts
+++ server/src/infrastructure/adapters/database/database.module.ts
@@ -1,33 +1,22 @@
-import { Module } from '@nestjs/common'
+import { Module, Provider } from '@nestjs/common'
 import { PrismaDatabaseService } from '@infrastructure/adapters/database/prisma.service'
 import { PrismaDatabaseTransaction } from '@infrastructure/adapters/database/transaction'
 import { Database } from '@domain/database'
 import { DatabaseTransaction } from '@domain/database/transaction'
 
+const providers: Provider[] = [
+  {
+    provide: Database,
+    useClass: PrismaDatabaseService
+  },
+  {
+    provide: DatabaseTransaction,
+    useClass: PrismaDatabaseTransaction
+  },
+]
+
 @Module({
-  providers: [
-    PrismaDatabaseService,
-    PrismaDatabaseTransaction,
-    {
-      provide: Database,
-      useClass: PrismaDatabaseService
-    },
-    {
-      provide: DatabaseTransaction,
-      useClass: PrismaDatabaseTransaction
-    },
-  ],
-  exports: [
-    PrismaDatabaseService,
-    PrismaDatabaseTransaction,
-    {
-      provide: Database,
-      useClass: PrismaDatabaseService
-    },
-    {
-      provide: DatabaseTransaction,
-      useClass: PrismaDatabaseTransaction
-    },
-  ]
+  providers: [...providers],
+  exports: [...providers]
 })
 export class InfrastructureDatabaseModule { }
