58a21d222dad9409e3aa96cd3e6cac1bcf76cbfb
Dan Ruan <danrsaquetti@gmail.com>
2024-12-26 23:45:20 -0300

    feat: Add new router: POST /bank-accounts/create

--- server/src/presentation/controllers/bank-account.controller.ts
+++ server/src/presentation/controllers/bank-account.controller.ts
@@ -1,6 +1,19 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { AuthGuard } from '@presentation/guards/auth.guard'
+import { BankAccountCreateUseCase } from '@application/use-cases/bank-account/create.use-case'
 
 @Controller('/bank-accounts')
 export class BankAccountController {
 
+  constructor(
+    private readonly bankAccountCreateUseCase: BankAccountCreateUseCase
+  ) { }
+
+  @UseGuards(AuthGuard)
+  @Post('/create')
+  async create(@Body() body: any) {
+    await this.bankAccountCreateUseCase.perform({ ...body })
+
+    return { message: 'Bank Account successfully created' }
+  }
 }
