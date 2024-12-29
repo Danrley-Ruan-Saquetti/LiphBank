cc2d9b06e18a9048b1c1683a21d5125480d7c88c
Dan Ruan <danrsaquetti@gmail.com>
2024-12-29 20:55:41 -0300

    refactor: Changing to use the @BankAccount decorator instead of receiving the bankAccountId as a parameter

--- server/src/presentation/controllers/financial-transaction.controller.ts
+++ server/src/presentation/controllers/financial-transaction.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, HttpCode, HttpStatus, Param, Post, UseGuards } from '@nestjs/common'
+import { BankAccount } from '@presentation/decorators/bank-account.decorator'
 import { AuthUserGuard } from '@presentation/guards/auth-user.guard'
 import { AuthBankAccountGuard } from '@presentation/guards/auth-bank-account.guard'
 import { FinancialTransactionCreateUseCase } from '@application/use-cases/financial-transaction/create.use-case'
@@ -14,8 +15,8 @@ export class FinancialTransactionController {
   @UseGuards(AuthBankAccountGuard)
   @HttpCode(HttpStatus.CREATED)
   @Post('/create')
-  async create(@Body() body: any) {
-    await this.financialTransactionCreateUseCase.perform({ ...body })
+  async create(@Body() body: any, @BankAccount() bankAccount: any) {
+    await this.financialTransactionCreateUseCase.perform({ ...body, bankAccountId: bankAccount.id })
 
     return { message: 'Financial Transaction successfully created' }
   }
