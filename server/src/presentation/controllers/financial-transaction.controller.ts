14e907656ef80157ffe290585f2e3f61b93826c1
Dan Ruan <danrsaquetti@gmail.com>
2024-12-29 21:07:47 -0300

    feat: Creating new type in presentation: BankAccountSession

--- server/src/presentation/controllers/financial-transaction.controller.ts
+++ server/src/presentation/controllers/financial-transaction.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpCode, HttpStatus, Param, Post, UseGuards } from '@nestjs/common'
 import { BankAccount } from '@presentation/decorators/bank-account.decorator'
 import { AuthUserGuard } from '@presentation/guards/auth-user.guard'
+import { BankAccountSession } from '@presentation/types/bank-account-session.type'
 import { AuthBankAccountGuard } from '@presentation/guards/auth-bank-account.guard'
 import { FinancialTransactionCreateUseCase } from '@application/use-cases/financial-transaction/create.use-case'
 
@@ -15,7 +16,7 @@ export class FinancialTransactionController {
   @UseGuards(AuthBankAccountGuard)
   @HttpCode(HttpStatus.CREATED)
   @Post('/create')
-  async create(@Body() body: any, @BankAccount() bankAccount: any) {
+  async create(@Body() body: any, @BankAccount() bankAccount: BankAccountSession) {
     await this.financialTransactionCreateUseCase.perform({ ...body, bankAccountId: bankAccount.id })
 
     return { message: 'Financial Transaction successfully created' }
