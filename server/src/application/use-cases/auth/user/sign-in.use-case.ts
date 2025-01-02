df26afd88433e0a335a07f3c7e068891fb083516
Dan Ruan <danrsaquetti@gmail.com>
2025-01-02 14:45:18 -0300

    Separating responsibilities into roles in AuthUserSignInUseCase

--- server/src/application/use-cases/auth/user/sign-in.use-case.ts
+++ server/src/application/use-cases/auth/user/sign-in.use-case.ts
@@ -5,6 +5,7 @@ import { AuthUserSignInEvent } from '@application/observer/events/auth/user/sign
 import { UnauthorizedException } from '@application/exceptions/unauthorized.exception'
 import { SignInCredentialInvalidException } from '@application/exceptions/sign-in-credential-invalid.exception'
 import { AuthUserSignInDTO, authUserSignInSchema } from '@application/dto/auth/user/sign-in.dto'
+import { User, UserType } from '@domain/entities/user.entity'
 import { JWTService } from '@domain/adapters/jwt/jwt.service'
 import { HashService } from '@domain/adapters/crypto/hash.service'
 import { UserRepository } from '@domain/repositories/user.repository'
@@ -24,39 +25,71 @@ export class AuthUserSignInUseCase extends UseCase<AuthUserSignInEvent> {
   async perform(args: AuthUserSignInDTO) {
     const { login, type, password } = this.validator.validate(authUserSignInSchema, args)
 
-    const user = await this.userRepository.findByLoginAndType(login, type)
+    const user = await this.getUser(login, type)
+
+    await this.validatePassword(user, password)
+    this.validateUserCanLogin(user)
+    await this.updateStateUserOnLogin(user)
+
+    const payload = this.createJWTPayloadUser(user)
+    const token = this.generateJWTToken(payload)
+
+    await this.notify('events.auth.user.sign-in', { user })
+
+    return { token, payload }
+  }
+
+  private async getUser(value: string, type: UserType) {
+    const [user] = await this.userRepository.findMany({
+      where: {
+        type,
+        OR: [
+          { login: value },
+          { code: value },
+        ]
+      },
+      take: 1,
+    })
 
     if (!user) {
       throw new SignInCredentialInvalidException()
     }
 
+    return user
+  }
+
+  private async validatePassword(user: User, password: string) {
     const isSamePassword = await this.hash.compare(password, user.password)
 
     if (!isSamePassword) {
       throw new SignInCredentialInvalidException()
     }
+  }
 
+  private validateUserCanLogin(user: User) {
     if (!user.active) {
       throw new UnauthorizedException('This currently inactive user account')
     }
+  }
 
+  private async updateStateUserOnLogin(user: User) {
     user.lastAccess = new Date(Date.now())
 
     await this.userRepository.update(user.id, user)
+  }
 
-    const payload: UserJWTPayload = {
+  private createJWTPayloadUser(user: User): UserJWTPayload {
+    return {
       sub: user.id,
       peopleId: user.peopleId,
       code: user.code,
     }
+  }
 
-    const token = this.jwt.encode(payload, {
+  private generateJWTToken(payload: UserJWTPayload) {
+    return this.jwt.encode(payload, {
       secret: env('JWT_USER_SECRET'),
       exp: env('JWT_USER_EXPIRATION'),
     })
-
-    await this.notify('events.auth.user.sign-in', { user })
-
-    return { token, payload }
   }
 }
