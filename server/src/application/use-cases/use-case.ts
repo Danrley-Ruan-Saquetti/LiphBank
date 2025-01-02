209dfcc0044f9fbd2f8399f0416422205b50b6d1
Dan Ruan <danrsaquetti@gmail.com>
2025-01-01 23:47:31 -0300

    feat: Implementing Observer resource access methods

--- server/src/application/use-cases/use-case.ts
+++ server/src/application/use-cases/use-case.ts
@@ -1,13 +1,32 @@
 import { Inject } from '@nestjs/common'
-import { ObserverService } from '@domain/adapters/observer/observer.service'
 import { ValidatorService } from '@domain/adapters/validator/validator.service'
+import { HandlerListener, IEventsType, ObserverService } from '@domain/adapters/observer/observer.service'
 
-export class UseCase {
+export class UseCase<Events extends IEventsType = any> {
 
   @Inject(ValidatorService)
   protected validator: ValidatorService
 
   @Inject(ObserverService)
-  protected observer: ObserverService
+  protected observer: ObserverService<Events>
 
+  subscribe<EventName extends keyof Events>(event: EventName, handler: HandlerListener<Events[EventName]>) {
+    return this.observer.subscribe(event, handler)
+  }
+
+  unsubscribe(id: string) {
+    return this.observer.unsubscribe(id)
+  }
+
+  protected async notify<EventName extends keyof Events>(event: EventName, data: Events[EventName]) {
+    return await this.observer.notify(event, data)
+  }
+
+  getListeners() {
+    return this.observer.getListeners()
+  }
+
+  getListenersByEvent(event: keyof Events) {
+    return this.observer.getListenersByEvent(event)
+  }
 }
