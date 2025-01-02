131c6d8da34cd43b8de030eb446b907966acdf9e
Dan Ruan <danrsaquetti@gmail.com>
2025-01-02 14:38:12 -0300

    refactor: Changing UserGenerateCodeUseCase and BankAccountGenerateCodeUseCase to use common use case GenerateCodeUseCase

--- server/src/application/use-cases/bank-account/generate-code.use-case.ts
+++ server/src/application/use-cases/bank-account/generate-code.use-case.ts
@@ -1,46 +1,27 @@
 import { Injectable } from '@nestjs/common'
-import { UseCase } from '@application/use-cases/use-case'
+import { GenerateCodeUseCase } from '@application/common/generate-code.use-case'
 import { CodeGenerationFailedException } from '@application/exceptions/code-generation-failed.exception'
-import { BankAccountGenerateCodeDTO, bankAccountGenerateCodeSchema } from '@application/dto/bank-account/generate-code.dto'
-import { CodeGeneratorService } from '@domain/adapters/generator/code/code.service'
 import { BankAccountRepository } from '@domain/repositories/bank-account.repository'
 
 @Injectable()
-export class BankAccountGenerateCodeUseCase extends UseCase {
+export class BankAccountGenerateCodeUseCase extends GenerateCodeUseCase {
 
   constructor(
     private readonly bankAccountRepository: BankAccountRepository,
-    private readonly codeGenerator: CodeGeneratorService
   ) {
     super()
+  }
 
+  onModuleInit() {
     this.codeGenerator.setLength(11)
     this.codeGenerator.setPrefix('BAK-')
   }
 
-  async perform(args: BankAccountGenerateCodeDTO = {}) {
-    const { attempts } = this.validator.validate(bankAccountGenerateCodeSchema, args)
-
-    const code = await this.generateCode(attempts)
-
-    return { code }
-  }
-
-  private async generateCode(attempts: number) {
-    for (let i = 0; i < attempts; i++) {
-      const code = this.codeGenerator.generate()
-
-      const isCodeAlreadyExists = await this.codeAlreadyExists(code)
-
-      if (!isCodeAlreadyExists) {
-        return code
-      }
-    }
-
-    throw new CodeGenerationFailedException('bank account')
-  }
-
-  private async codeAlreadyExists(code: string) {
+  protected async validateCode(code: string) {
     return !(await this.bankAccountRepository.findByCode(code))
+  }
+
+  protected async attemptsExceeded() {
+    throw new CodeGenerationFailedException('bank account')
   }
 }
--- server/src/application/use-cases/user/generate-code.use-case.ts
+++ server/src/application/use-cases/user/generate-code.use-case.ts
@@ -1,46 +1,27 @@
 import { Injectable } from '@nestjs/common'
-import { UseCase } from '@application/use-cases/use-case'
+import { GenerateCodeUseCase } from '@application/common/generate-code.use-case'
 import { CodeGenerationFailedException } from '@application/exceptions/code-generation-failed.exception'
-import { UserGenerateCodeDTO, userGenerateCodeSchema } from '@application/dto/user/generate-code.dto'
 import { UserRepository } from '@domain/repositories/user.repository'
-import { CodeGeneratorService } from '@domain/adapters/generator/code/code.service'
 
 @Injectable()
-export class UserGenerateCodeUseCase extends UseCase {
+export class UserGenerateCodeUseCase extends GenerateCodeUseCase {
 
   constructor(
     private readonly userRepository: UserRepository,
-    private readonly codeGenerator: CodeGeneratorService
   ) {
     super()
+  }
 
+  onModuleInit() {
     this.codeGenerator.setLength(11)
     this.codeGenerator.setPrefix('USR-')
   }
 
-  async perform(args: UserGenerateCodeDTO = {}) {
-    const { attempts } = this.validator.validate(userGenerateCodeSchema, args)
-
-    const code = await this.generateCode(attempts)
-
-    return { code }
-  }
-
-  private async generateCode(attempts: number) {
-    for (let i = 0; i < attempts; i++) {
-      const code = this.codeGenerator.generate()
-
-      const isCodeAlreadyExists = await this.codeAlreadyExists(code)
-
-      if (!isCodeAlreadyExists) {
-        return code
-      }
-    }
-
-    throw new CodeGenerationFailedException('user')
-  }
-
-  private async codeAlreadyExists(code: string) {
+  protected async validateCode(code: string) {
     return !(await this.userRepository.findByCode(code))
+  }
+
+  protected async attemptsExceeded() {
+    throw new CodeGenerationFailedException('Method not implemented.')
   }
 }
