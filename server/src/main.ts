034865fdabb4baf7900732ea04ef496c03c78d03
Dan Ruan <danrsaquetti@gmail.com>
2025-01-02 15:51:27 -0300

    feat: Enabling error only logging in the nest application

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,9 @@ import { MainModule } from '@main.module'
 import { env } from '@shared/env'
 
 async function bootstrap() {
-  const app = await NestFactory.create(MainModule)
+  const app = await NestFactory.create(MainModule, { logger: ['error'] })
   await app.listen(env('PORT'))
+
+  console.log('Server started!')
 }
 bootstrap()
