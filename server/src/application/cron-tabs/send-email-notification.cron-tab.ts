f23940536c90ab990a87f9bc27a65cd39b954f44
Dan Ruan <danrsaquetti@gmail.com>
2025-01-02 12:53:04 -0300

    feat: Updating EmailNotification status when notification has finished sending

--- server/src/application/cron-tabs/send-email-notification.cron-tab.ts
+++ server/src/application/cron-tabs/send-email-notification.cron-tab.ts
@@ -3,6 +3,7 @@ import { Cron, CronExpression } from '@nestjs/schedule'
 import { MailService } from '@domain/adapters/mail/mail.service'
 import { NotificationSituation } from '@domain/entities/notification.entity'
 import { EmailNotificationRepository } from '@domain/repositories/email-notification.repository'
+import { EmailNotification } from '@domain/entities/email-notification.entity'
 
 @Injectable()
 export class SendEmailNotificationCronTab {
@@ -14,18 +15,50 @@ export class SendEmailNotificationCronTab {
 
   @Cron(CronExpression.EVERY_10_SECONDS)
   async sendEmailNotification() {
-    const notificationInQueue = await this.emailNotificationRepository.findMany({
-      where: {
-        situation: NotificationSituation.IN_QUEUE
-      },
+    const notificationInQueue = await this.getEmailNotificationsInQueue()
+
+    for (let i = 0; i < notificationInQueue.length; i++) {
+      const isSuccess = await this.sendEmail(notificationInQueue[i])
+
+      await this.updateSituationNotification(isSuccess, notificationInQueue[i])
+    }
+  }
+
+  private async getEmailNotificationsInQueue() {
+    return await this.emailNotificationRepository.findMany({
+      where: { situation: NotificationSituation.IN_QUEUE },
       orderBy: { id: 'asc' },
       take: 100
     })
+  }
 
-    for (let i = 0; i < notificationInQueue.length; i++) {
-      const { sender, recipient, body, subject } = notificationInQueue[i]
+  private async sendEmail({ sender, recipient, body, subject }: EmailNotification) {
+    try {
+      await this.mailService.send({ from: sender, to: recipient.split(','), subject, body })
 
-      this.mailService.send({ from: sender, to: recipient.split(','), subject, body })
+      return true
+    } catch (error: any) {
+      return false
     }
+  }
+
+  private async updateSituationNotification(isSuccess: boolean, emailNotification: EmailNotification) {
+    if (isSuccess) emailNotification = await this.updateStateErrorSendNotification(emailNotification)
+    else emailNotification = await this.updateStateSuccessSendNotification(emailNotification)
+
+    await this.emailNotificationRepository.update(emailNotification.id, emailNotification)
+  }
+
+  private async updateStateErrorSendNotification(emailNotification: EmailNotification) {
+    emailNotification.situation = NotificationSituation.ERROR
+
+    return emailNotification
+  }
+
+  private async updateStateSuccessSendNotification(emailNotification: EmailNotification) {
+    emailNotification.situation = NotificationSituation.SENT
+    emailNotification.sendAt = new Date(Date.now())
+
+    return emailNotification
   }
 }
