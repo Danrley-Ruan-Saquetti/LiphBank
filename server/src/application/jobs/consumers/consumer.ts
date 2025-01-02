e7daadceb107ebb3e39862f5b1b24809217f8779
Dan Ruan <danrsaquetti@gmail.com>
2025-01-02 20:26:45 -0300

    feat: Using ErrorLogService in QueueConsumer

--- server/src/application/jobs/consumers/consumer.ts
+++ server/src/application/jobs/consumers/consumer.ts
@@ -1,10 +1,24 @@
+import { Inject } from '@nestjs/common'
 import { OnQueueFailed } from '@nestjs/bull'
 import { Job } from 'bull'
+import { ErrorLogService } from '@domain/adapters/error-log/error-log.service'
 
 export class QueueConsumer {
 
+  @Inject(ErrorLogService)
+  private errorLogService: ErrorLogService
+
   @OnQueueFailed()
-  handler(job: Job, error: Error) {
-    console.log(`Job name: "${job.name}"`, error)
+  async handler(job: Job, error: any) {
+    await this.errorLogService.save({
+      origin: 'JOB',
+      message: error.message,
+      details: {
+        ...error.details,
+        JOB: {
+          name: job.name,
+        }
+      }
+    })
   }
 }
