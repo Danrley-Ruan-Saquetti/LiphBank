58c4528937a1e5bdfa2c9e70826af5a1b308fa5a
Dan Ruan <danrsaquetti@gmail.com>
2025-01-02 19:34:23 -0300

    feat: Implementing ErrorLogRepositoryImplementation

--- server/src/infrastructure/repositories/repository.module.ts
+++ server/src/infrastructure/repositories/repository.module.ts
@@ -2,12 +2,14 @@ import { Module, Provider } from '@nestjs/common'
 import { InfrastructureDatabaseModule } from '@infrastructure/adapters/database/database.module'
 import { UserRepositoryImplementation } from '@infrastructure/repositories/user.repository'
 import { PeopleRepositoryImplementation } from '@infrastructure/repositories/people.repository'
+import { ErrorLogRepositoryImplementation } from '@infrastructure/repositories/error-log.repository'
 import { BankAccountRepositoryImplementation } from '@infrastructure/repositories/bank-account.repository'
 import { NotificationRepositoryImplementation } from '@infrastructure/repositories/notification.repository'
 import { EmailNotificationRepositoryImplementation } from '@infrastructure/repositories/email-notification.repository'
 import { FinancialTransactionRepositoryImplementation } from '@infrastructure/repositories/financial-transaction.repository'
 import { UserRepository } from '@domain/repositories/user.repository'
 import { PeopleRepository } from '@domain/repositories/people.repository'
+import { ErrorLogRepository } from '@domain/repositories/error-log.repository'
 import { BankAccountRepository } from '@domain/repositories/bank-account.repository'
 import { NotificationRepository } from '@domain/repositories/notification.repository'
 import { EmailNotificationRepository } from '@domain/repositories/email-notification.repository'
@@ -37,6 +39,10 @@ const providers: Provider[] = [
   {
     provide: NotificationRepository,
     useClass: NotificationRepositoryImplementation
+  },
+  {
+    provide: ErrorLogRepository,
+    useClass: ErrorLogRepositoryImplementation
   },
 ] as const
 
