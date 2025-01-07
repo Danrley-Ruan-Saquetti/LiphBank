27ec8d55103ee1f8d3797e8f17bdd9ac78834cbc
Dan Ruan <danrsaquetti@gmail.com>
2025-01-06 22:40:36 -0300

    feat: Creating new routers: POST "/bank-accounts/financial-transactions/:id/conclude" and POST "/bank-accounts/financial-transactions/:id/cancel"

--- server/src/presentation/controllers/financial-transaction.controller.ts
+++ server/src/presentation/controllers/financial-transaction.controller.ts
@@ -3,8 +3,11 @@ import { BankAccount } from '@presentation/decorators/bank-account.decorator'
 import { AuthUserGuard } from '@presentation/guards/auth-user.guard'
 import { BankAccountSession } from '@presentation/types/bank-account-session.type'
 import { AuthBankAccountGuard } from '@presentation/guards/auth-bank-account.guard'
+import { UpdateBalanceBankAccountListener } from '@application/observer/listeners/update-balance-bank-account.listener'
 import { FinancialTransactionQueryUseCase } from '@application/use-cases/financial-transaction/query.use-case'
+import { FinancialTransactionCancelUseCase } from '@application/use-cases/financial-transaction/cancel.use-case'
 import { FinancialTransactionCreateUseCase } from '@application/use-cases/financial-transaction/create.use-case'
+import { FinancialTransactionConcludeUseCase } from '@application/use-cases/financial-transaction/conclude.use-case'
 
 @Controller('/bank-accounts/financial-transactions')
 export class FinancialTransactionController {
@@ -12,6 +15,9 @@ export class FinancialTransactionController {
   constructor(
     private readonly financialTransactionCreateUseCase: FinancialTransactionCreateUseCase,
     private readonly financialTransactionQueryUseCase: FinancialTransactionQueryUseCase,
+    private readonly financialTransactionConcludeUseCase: FinancialTransactionConcludeUseCase,
+    private readonly financialTransactionCancelUseCase: FinancialTransactionCancelUseCase,
+    private readonly updateBalanceBankAccountListener: UpdateBalanceBankAccountListener,
   ) { }
 
   @UseGuards(AuthUserGuard)
@@ -31,5 +37,28 @@ export class FinancialTransactionController {
     await this.financialTransactionCreateUseCase.perform({ ...body, bankAccountId: bankAccount.id })
 
     return { message: 'Financial Transaction successfully created' }
+  }
+
+  @UseGuards(AuthUserGuard)
+  @UseGuards(AuthBankAccountGuard)
+  @Post('/:id/conclude')
+  async conclude(@BankAccount() bankAccount: BankAccountSession, @Param('id') financialTransactionId: number) {
+    this.financialTransactionConcludeUseCase.observer.subscribe(
+      'events.financial-transaction.conclude',
+      async data => await this.updateBalanceBankAccountListener.perform(data)
+    )
+
+    await this.financialTransactionConcludeUseCase.perform({ bankAccountId: bankAccount.id, financialTransactionId })
+
+    return { message: 'Financial Transaction successfully completed' }
+  }
+
+  @UseGuards(AuthUserGuard)
+  @UseGuards(AuthBankAccountGuard)
+  @Post('/:id/cancel')
+  async cancel(@BankAccount() bankAccount: BankAccountSession, @Param('id') financialTransactionId: number) {
+    await this.financialTransactionCancelUseCase.perform({ bankAccountId: bankAccount.id, financialTransactionId })
+
+    return { message: 'Financial Transaction successfully canceled' }
   }
 }
