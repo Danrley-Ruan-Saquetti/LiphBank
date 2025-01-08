3dd2f03d35b5f5202e974f13f44a367e4b428336
Dan Ruan <danrsaquetti@gmail.com>
2025-01-07 22:26:18 -0300

    feat: Creating new value object: DefineInitialSituationFinancialTransactionValueObject

---
+++ server/src/domain/value-objects/define-initial-situation-financial-transaction.value-object.ts
@@ -0,0 +1,16 @@
+import { FinancialTransactionProps, FinancialTransactionSituation } from '@domain/entities/financial-transaction.entity'
+
+export class DefineInitialSituationFinancialTransactionValueObject {
+
+  constructor(
+    private readonly financialTransaction: { expiresIn?: Date | null }
+  ) { }
+
+  getSituation(): FinancialTransactionSituation {
+    if (this.financialTransaction.expiresIn && this.financialTransaction.expiresIn < new Date(Date.now())) {
+      return FinancialTransactionSituation.LATE
+    }
+
+    return FinancialTransactionSituation.PENDING
+  }
+}
