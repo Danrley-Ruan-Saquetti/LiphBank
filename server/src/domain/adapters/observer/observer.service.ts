3bbb55de0c491cd3b59a7440a266c8b98269be85
Dan Ruan <danrley@pontogovsistemas.com.br>
2025-01-09 09:00:16 -0300

    refactor: Implementing interfaces of the observer

--- server/src/domain/adapters/observer/observer.service.ts
+++ server/src/domain/adapters/observer/observer.service.ts
@@ -1,11 +1,10 @@
-import { IEventsType, SubscriberListener } from '@domain/adapters/observer/interfaces'
-import { Listener, IListener, ListenerActionPerformed } from '@domain/adapters/observer/listener'
+import { Listener } from '@domain/adapters/observer/listener'
+import { IEventsType, IListener, IPublisher, ISubscriber, SubscriberListener } from '@domain/adapters/observer/interfaces'
 
-
-export abstract class ObserverService<Events extends IEventsType = any> {
+export abstract class ObserverService<Events extends IEventsType = any> implements IPublisher<Events>, ISubscriber<Events> {
 
   abstract subscribe<EventName extends keyof Events>(event: EventName, listenerHandle: SubscriberListener<Events[EventName]>): Listener<Events[EventName]>
-  abstract unsubscribe(listener: Listener): boolean
+  abstract unsubscribe(listener: IListener): boolean
   abstract notify<EventName extends keyof Events>(event: EventName, data: Events[EventName]): Promise<void>
   abstract getListeners(): Listener[]
   abstract getListenersByEvent(event: keyof Events): Listener[]
