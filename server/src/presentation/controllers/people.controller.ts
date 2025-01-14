0ded41304c688492f498950a316371e01989b005
Dan Ruan <danrsaquetti@gmail.com>
2025-01-13 22:00:50 -0300

    feat: Creating new router: PUT "/peoples/update"

--- server/src/presentation/controllers/people.controller.ts
+++ server/src/presentation/controllers/people.controller.ts
@@ -1,6 +1,21 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Put, UseGuards } from '@nestjs/common'
+import { PeopleUpdateUseCase } from '@application/use-cases/people/update.use-case'
+import { User } from '@presentation/decorators/user.decorator'
+import { UserSession } from '@presentation/types/user-session.type'
+import { AuthUserGuard } from '@presentation/guards/auth-user.guard'
 
 @Controller('/peoples')
 export class PeopleController {
 
+  constructor(
+    private readonly peopleUpdateUseCase: PeopleUpdateUseCase
+  ) { }
+
+  @UseGuards(AuthUserGuard)
+  @Put('/update')
+  async update(@Body() body: any, @User() user: UserSession) {
+    await this.peopleUpdateUseCase.perform({ ...body, id: user.peopleId })
+
+    return { message: 'People successfully updated' }
+  }
 }
