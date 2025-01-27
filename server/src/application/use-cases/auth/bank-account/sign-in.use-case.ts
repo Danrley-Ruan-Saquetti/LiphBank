aca7528f14849d473203e46a7f5088386b9d00dd
Dan Ruan <danrsaquetti@gmail.com>
2025-01-27 20:25:56 -0300

    refactor: Refactoring AuthBankAccountSignInUseCase use case to separate responsibilities into small functions

--- server/src/application/use-cases/auth/bank-account/sign-in.use-case.ts
+++ server/src/application/use-cases/auth/bank-account/sign-in.use-case.ts
@@ -6,6 +6,7 @@ import { BankAccountJWTPayload } from '@application/types/bank-account-jwt-paylo
 import { AuthBankAccountSignInEvent } from '@application/observer/events/auth/bank-account/sign-in.event'
 import { AuthBankAccountSignInDTO, authBankAccountSignInSchema } from '@application/dto/auth/bank-account/sign-in.dto'
 import { JWTService } from '@domain/adapters/jwt/jwt.service'
+import { BankAccount } from '@domain/entities/bank-account.entity'
 import { BankAccountRepository } from '@domain/repositories/bank-account.repository'
 import { env } from '@shared/env'
 
@@ -22,6 +23,16 @@ export class AuthBankAccountSignInUseCase extends UseCase<AuthBankAccountSignInE
   async perform(args: AuthBankAccountSignInDTO) {
     const { peopleId, code } = this.validator.validate(authBankAccountSignInSchema, args)
 
+    const bankAccount = await this.findBankAccount(code, peopleId)
+    const payload = this.createJWTPayloadBankAccount(bankAccount)
+    const token = this.generateJWTToken(payload)
+
+    await this.observer.notify('events.auth.bank-account.sign-in', { bankAccount })
+
+    return { token, payload }
+  }
+
+  private async findBankAccount(code: string, peopleId: number) {
     const bankAccount = await this.bankAccountRepository.findByCode(code)
 
     if (!bankAccount) {
@@ -36,18 +47,20 @@ export class AuthBankAccountSignInUseCase extends UseCase<AuthBankAccountSignInE
       throw new UnauthorizedException('This currently inactive bank account')
     }
 
-    const payload: BankAccountJWTPayload = {
+    return bankAccount
+  }
+
+  private createJWTPayloadBankAccount(bankAccount: BankAccount): BankAccountJWTPayload {
+    return {
       sub: bankAccount.id,
       code: bankAccount.code,
     }
+  }
 
-    const token = this.jwt.encode(payload, {
-      exp: env('JWT_BANK_ACCOUNT_EXPIRATION'),
-      secret: env('JWT_BANK_ACCOUNT_SECRET'),
+  private generateJWTToken(payload: BankAccountJWTPayload) {
+    return this.jwt.encode(payload, {
+      secret: env('JWT_USER_SECRET'),
+      exp: env('JWT_USER_EXPIRATION'),
     })
-
-    await this.observer.notify('events.auth.bank-account.sign-in', { bankAccount })
-
-    return { token, payload }
   }
 }
